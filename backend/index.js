97c464c2ea1971ed83522672ee996b1b18cc4e34
Gabriel-Savoldi <gabrielbaldassarini@gmail.com>
2025-03-21 08:46:52 -0300

    tentando usar Nodemailer

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,19 @@
-const express = require('express');
-const cors = require('cors');
+import nodemailer from 'nodemailer'
+import express from 'express'
+import cors from 'cors'
+
 const app = express();
 const PORT = 5000;
+const transporter = nodemailer.createTransport({
+  host:'live.smtp.mailtrap.io',
+  port:587,
+  secure:'false',
+  auth: {
+    user: 'sossistema1@gmail.com',  // Use variáveis de ambiente para armazenar dados sensíveis
+    pass: 'sosadministrador2003',  // Use variáveis de ambiente para armazenar dados sensíveis
+  }
+
+})
 
 app.use(cors());
 app.use(express.json());
@@ -13,3 +25,8 @@ app.get('/api', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.post('/login', (req, res) => {
+  res.json({ message: 'Hello from the backend!' });
+});
+
