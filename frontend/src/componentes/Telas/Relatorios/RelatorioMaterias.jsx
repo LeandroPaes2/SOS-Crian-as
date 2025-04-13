ecd6d8a20815fdee8f14fc7b8b92c1296247a8bd
LeonardoPlins <leonardoplins181@gmail.com>
2025-04-13 10:53:03 -0300

    Atualizar Materia Funcional

--- frontend/src/componentes/Telas/Relatorios/RelatorioMaterias.jsx
+++ frontend/src/componentes/Telas/Relatorios/RelatorioMaterias.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Container, Table, Button, Form, InputGroup, Alert } from "react-bootstrap";
 import PaginaGeral from "../../layouts/PaginaGeral";
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 export default function RelatorioMateria(){
 
@@ -10,6 +10,7 @@ export default function RelatorioMateria(){
     const [nome, setNome] = useState("");
     const [descricao, setDescricao] = useState("");
     const [pesquisaNome, setPesquisaNome] = useState("");
+    const navigate = useNavigate();
 
     useEffect(() => {
         const buscarMateria = async () => {
@@ -140,11 +141,7 @@ export default function RelatorioMateria(){
                                             <td>{materia.nome}</td>
                                             <td>{materia.descricao}</td>
                                             <td>
-                                                <Button 
-                                                    as={Link} 
-                                                    to={{
-                                                        pathname: "/cadastroMateria",
-                                                        state: { nome: materia.nome, descricao: materia.descricao}}}variant="warning">
+                                                <Button onClick={() => navigate("/cadastroMateria",  { state: {nome: materia.nome, descricao: materia.descricao}})}variant="warning">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
                                                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                                                         <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
