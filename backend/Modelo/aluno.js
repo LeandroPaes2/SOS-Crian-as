782075409205ae852d61e6b5632160645e915a55
Gabriel-Savoldi <gabrielbaldassarini@gmail.com>
2025-04-15 07:29:32 -0300

    Aluno nn funfa mas vai

--- backend/Modelo/aluno.js
+++ backend/Modelo/aluno.js
@@ -1,41 +1,75 @@
 import AlunoDAO from "../Persistencia/alunoDAO.js";
+import Responsavel from "./responsavel.js";
+import Escola from "./escola.js";
 
 export default class Aluno {
-    #id;
+    #numProtocolo;
     #nome;
-    #idade;
+    #dataNascimento;
     #responsavel;
-    #endereco;
+    #rua;
+    #bairro;
+    #cidade;
+    #cep;
+    #numero;
+    #escola
     #telefone;
     #periodoProjeto;
     #periodoEscola;
+    #realizaAcompanhamento;
+    #possuiSindrome;
 
-    constructor(nome = "", idade = "", responsavel = "", endereco = "", telefone = "", periodoProjeto = "", periodoEscola = "", id = 0) {
-        this.#id = id;
+    constructor(numProtocolo=0, nome="", dataNascimento="", responsavel={},rua="", bairro="", cidade="", cep="", numero="", escola={} ,telefone="", periodoProjeto="", periodoEscola="", realizaAcompanhamento="", possuiSindrome="") {
+        this.#numProtocolo = numProtocolo;
         this.#nome = nome;
-        this.#idade = idade;
+        this.#dataNascimento = dataNascimento;
         this.#responsavel = responsavel;
-        this.#endereco = endereco;
+        this.#rua = rua;
+        this.#bairro = bairro;
+        this.#cidade = cidade;
+        this.#cep = cep;
+        this.#numero = numero;
+        this.#escola = escola;
         this.#telefone = telefone;
         this.#periodoProjeto = periodoProjeto;
         this.#periodoEscola = periodoEscola;
+        this.#realizaAcompanhamento = realizaAcompanhamento;
+        this.#possuiSindrome = possuiSindrome;
     }
 
-    get id() { return this.#id; }
-    set id(valor) { this.#id = valor; }
+    get numProtocolo() { return this.#numProtocolo; }
+    set numProtocolo(valor) { this.#numProtocolo = valor; }
 
     get nome() { return this.#nome; }
     set nome(valor) { this.#nome = valor; }
 
-    get idade() { return this.#idade; }
-    set idade(valor) { this.#idade = valor; }
+   get dataNascimento() { return this.#dataNascimento; }
+    set dataNascimento(valor) { this.#dataNascimento = valor; }
 
     get responsavel() { return this.#responsavel; }
-    set responsavel(valor) { this.#responsavel = valor; }
+    set responsavel(novoResponsavel) {
+        if(novoResponsavel instanceof Responsavel)
+            this.#responsavel = novoResponsavel;
+        }
 
-    get endereco() { return this.#endereco; }
-    set endereco(valor) { this.#endereco = valor; }
+    get rua() { return this.#rua; }
+    set rua(valor) { this.#rua = valor; }
 
+    get bairro() { return this.#bairro; }    
+    set bairro(valor) { this.#bairro = valor; }
+
+    get cidade() { return this.#cidade; }
+    set cidade(valor) { this.#cidade = valor; }
+
+    get cep() { return this.#cep; }
+    set cep(valor) { this.#cep = valor; }
+
+    get numero() { return this.#numero; }
+    set numero(valor) { this.#numero = valor; }
+
+    get escola() { return this.#escola; }
+    set escola(novaEscola) { this.#escola = novaEscola; }
+    
     get telefone() { return this.#telefone; }
     set telefone(valor) { this.#telefone = valor; }
 
@@ -45,16 +79,33 @@ export default class Aluno {
     get periodoEscola() { return this.#periodoEscola; }
     set periodoEscola(valor) { this.#periodoEscola = valor; }
 
+    get realizaAcompanhamento() { return this.#realizaAcompanhamento; }
+    set realizaAcompanhamento(valor) { this.#realizaAcompanhamento = valor; }
+
+    get possuiSindrome() { return this.#possuiSindrome; }
+    set possuiSindrome(valor) { this.#possuiSindrome = valor; }
+
+
+
+
+
     toJSON() {
         return {
-            id: this.#id,
+            numProtocolo: this.#numProtocolo,
             nome: this.#nome,
-            idade: this.#idade,
-            responsavel: this.#responsavel,
-            endereco: this.#endereco,
+            dataNascimento: this.#dataNascimento,
+            responsavel: this.#responsavel.toJSON(),
+            rua: this.#rua,
+            bairro: this.#bairro,
+            cidade: this.#cidade,
+            cep: this.#cep,
+            numero: this.#numero,
+            escola: this.#escola.toJSON(),
             telefone: this.#telefone,
             periodoProjeto: this.#periodoProjeto,
-            periodoEscola: this.#periodoEscola
+            periodoEscola: this.#periodoEscola,
+            realizaAcompanhamento: this.#realizaAcompanhamento,
+            possuiSindrome: this.#possuiSindrome
         };
     }
 
