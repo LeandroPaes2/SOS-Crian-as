e12e3298e4ed691af6313624d5dff440ec135400
LeandroPaes2 <lelepaes205@gmail.com>
2025-04-21 00:41:19 -0300

    Tentando deixar bonito 2.0

--- frontend/src/componentes/layouts/Menu.jsx
+++ frontend/src/componentes/layouts/Menu.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import {Link} from 'react-router-dom';
-import logo from '../../logo.png';
+import logo from '../imagens/logo.png';
 import "../css/menu.css";
 export default function Menu(props){
     return(
