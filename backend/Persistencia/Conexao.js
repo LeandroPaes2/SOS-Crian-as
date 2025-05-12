9fa6f4305348ad9aaaf435fa38fbec067087099f
IsabellyMV2004 <isabellymarafaovoltareli2004@gmail.com>
2025-05-12 09:12:49 -0300

    aaaa

--- backend/Persistencia/Conexao.js
+++ backend/Persistencia/Conexao.js
@@ -1,20 +1,21 @@
 import pg from 'pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  connectionString: process.env.SUPABASE_URL_CONEXAO,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 30000,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+let pool;
 
 export default async function conectar() {
-  return await pool.connect();
+  if (!pool) {
+    pool = new Pool({
+      connectionString: process.env.SUPABASE_URL_CONEXAO,
+      max: 20,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 60000,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+  }
+
+  return await pool.connect(); // client com `.release()`
 }
